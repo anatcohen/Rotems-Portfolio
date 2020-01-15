7b95dc8487981f211ced4d78a3052085a43e2057
Anat <anatco13@gmail.com>
2020-01-15 15:50:22 +0200

    added local storage

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import * as firebase from 'firebase';
 import Item from './Item';
+import { LS_COLLECTION } from './UploadeForm';
+
 export default function Portfolio() {
     const [data, setData] = useState([]),
         // Changes item's view to either grid or list view
@@ -8,12 +10,15 @@ export default function Portfolio() {
             document.getElementById('item-container').style.flexDirection = e.target.id;
         }
 
-    // Adds checkboxs to the type
-    /*
-    ,addCheckBoxs = () => {
+        // Adds checkboxs to the type
 
-    }
-    */
+        , addCheckBoxs = () => {
+            let arrTemp = JSON.parse(localStorage.getItem(LS_COLLECTION));
+            return <> {arrTemp.map((type, index) => {
+                return <div key={index} > {type} < input type="checkbox" id={type} /></div>
+            })
+            }</>;
+        }
 
     useEffect(() => {
         firebase.firestore().collection("portfolio").get().then(querySnapshot => {
@@ -35,6 +40,7 @@ export default function Portfolio() {
                     <button id='column' />
                 </div>
                 <>
+                    {addCheckBoxs()}
                 </>
             </div>
             <div id="item-container">
--- src/UploadeForm.js
+++ src/UploadeForm.js
@@ -1,33 +1,91 @@
 import React, { useState } from 'react';
 import * as firebase from 'firebase';
+export const LS_COLLECTION = 'arrCollections';
 
 export default function UploadForm(props) {
     const [loading, setLoading] = useState(false);
-
     const onFormSubmit = e => {
         e.preventDefault();
+        let fileName = document.getElementById('name').value,
+            fileType = document.getElementById('type').value,
+            storageRef = firebase.storage().ref().child(fileName);
+
         setLoading(true);
-        let storageRef = firebase.storage().ref().child(document.getElementById('name').value);
         //Adds image to storage 
         storageRef.put(document.getElementById('file').files[0]).then(snapshot => {
             // Gets image's url
             storageRef.getDownloadURL().then(res => {
                 // Adds to database
-                firebase.firestore().collection(document.getElementById('type').value).add({
-                    name: document.getElementById('name').value,
-                    type: document.getElementById('type').value,
+                firebase.firestore().collection('portfolio').add({
+                    name: fileName,
+                    type: fileType,
                     description: document.getElementById('description').value,
-                    url: res
+                    url: res,
+                    time: setDate(new Date())
                 });
             });
             setLoading(false);
+            // Adds collection's name to localStorage
+            let arrTemp = JSON.parse(localStorage.getItem(LS_COLLECTION));
+            if (arrTemp === null) arrTemp = [fileType];
+            else if (arrTemp.filter(name => name === fileType).length === 0) arrTemp.push(fileType);
+            localStorage.setItem(LS_COLLECTION, JSON.stringify(arrTemp));
+            console.log(localStorage.getItem(LS_COLLECTION));
         });
-
     }, onFormChange = e => {
         let bAreFieldsEmpty = true
         if (document.getElementById('file').value !== "" && document.getElementById('name').value !== "" && document.getElementById('type').value !== "") bAreFieldsEmpty = false;
         document.getElementById('submitBtn').style.visibility = bAreFieldsEmpty ? 'hidden' : 'visible';
-    }
+    },
+        // Returns today's full date 
+        setDate = date => {
+            // Gets day
+            let retDate = date.getDate();
+            // Gets month
+            switch (date.getMonth()) {
+                case 0:
+                    retDate += ' January ';
+                    break;
+                case 1:
+                    retDate += ' February ';
+                    break;
+                case 2:
+                    retDate += ' March ';
+                    break;
+                case 3:
+                    retDate += ' April ';
+                    break;
+                case 4:
+                    retDate += ' May ';
+                    break;
+                case 5:
+                    retDate += ' June ';
+                    break;
+                case 6:
+                    retDate += ' July ';
+                    break;
+                case 7:
+                    retDate += ' August ';
+                    break;
+                case 8:
+                    retDate += ' September ';
+                    break;
+                case 9:
+                    retDate += ' October ';
+                    break;
+                case 10:
+                    retDate += ' November ';
+                    break;
+                case 11:
+                    retDate += ' December ';
+                    break;
+                default: break;
+            }
+            // Gets year
+            retDate += date.getFullYear();
+            return retDate;
+        }
+
     return (
         <>
             <p>Upload an item: </p>
